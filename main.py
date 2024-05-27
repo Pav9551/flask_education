1b27943283cdbc22817caaa84692c4b56c4a2aab
pav9551 <pav9551@yandex.ru>
2024-05-27 17:48:11 +0300

    added index.html

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+def print_name():
+    print(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Hello World!</h1>'
+@app.route('/user/<name>')
+def user(name):
+    if name == 'pavel':
+        print('Hi, pavel')
+    #return '<h1>Hello, %s - user!</h1>' % name
+    return render_template('index.html', name = 'my name')
+
+if __name__ == '__main__':
+    print_name()
+    app.run(debug=True)
+    #pass
+#print_name()
